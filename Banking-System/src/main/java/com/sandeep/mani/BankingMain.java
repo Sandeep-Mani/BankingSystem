3b791af69a9596c6a32ea0b083ff142b7bc2e0a9
Sandeep-Mani <sandeepmani2312@gmail.com>
2020-07-18 03:06:56 +0530

    Added spring jars and created spring application class

---
+++ Banking-System/src/main/java/com/sandeep/mani/BankingMain.java
@@ -0,0 +1,11 @@
+package com.sandeep.mani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingMain {
+	public static void main(String[] args) {
+		SpringApplication.run(BankingMain.class, args);
+	}
+}
