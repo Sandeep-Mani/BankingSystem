cd1aee7371857407ba62c6a72c627ff833571369
Sandeep-Mani <sandeepmani2312@gmail.com>
2020-07-25 01:24:48 +0530

    Anotation update

--- Banking-System/src/main/java/com/sandeep/mani/users/UserController.java
+++ Banking-System/src/main/java/com/sandeep/mani/users/UserController.java
@@ -3,10 +3,12 @@ package com.sandeep.mani.users;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,27 +17,27 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping("/users")
+	@GetMapping("/users")
 	public List<User> getAllUsers() {		
 		return userService.getAllTopics();
 	}
 	
-	@RequestMapping("/users/{userId}")
+	@GetMapping("/users/{userId}")
 	public User getUser(@PathVariable String userId) {
 		return userService.getUser(userId);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/users")
+	@PostMapping("/users")
 	public void addUser(@RequestBody User user) {
 		userService.addUser(user);
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT, value = "/users/{userId}")
+	@PutMapping("/users/{userId}")
 	public void updateUser(@RequestBody User user, @PathVariable String userId) {
 		userService.updateUser(userId, user);
 	}
 	
-	@RequestMapping(method = RequestMethod.DELETE, value = "/users/{userId}")
+	@DeleteMapping("/users/{userId}")
 	public void removeUser(@PathVariable String userId) {
 		userService.removeUser(userId);
 	}
