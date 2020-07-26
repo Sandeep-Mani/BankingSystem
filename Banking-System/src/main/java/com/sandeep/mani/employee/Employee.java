faabe29ad4b5cfda75c608461a8d06d3ef5f3bc3
Sandeep-Mani <sandeepmani2312@gmail.com>
2020-07-27 01:05:37 +0530

    Employee abstract class

--- Banking-System/src/main/java/com/sandeep/mani/employee/Employee.java
+++ Banking-System/src/main/java/com/sandeep/mani/employee/Employee.java
@@ -1,5 +1,84 @@
 package com.sandeep.mani.employee;
 
-public class Employee {
+import java.sql.Date;
+import java.util.UUID;
 
+public abstract class Employee {
+	private UUID id;
+	private String email;
+	private String nationality;
+	private String userName;
+	private String password;
+	private String name;
+	private String salary;
+	private Date dob;
+	private String contact;
+	private String gender;
+	private String address;
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}	
 }
